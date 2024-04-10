1bec5a112295d2f13dcb43030a35f8e2e11b44e9
Abhikr-1871 <165637203+Abhikr-1871@users.noreply.github.com>
2024-04-11 04:58:49 +0530

    Add files via upload

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -1,1 +1,22 @@
+// import logo from './logo.svg';
 
+import Header from '../header';
+import Searchbar from "../searchbar"
+import Carousel from '../carousel';
+import Features from "../features"
+function Home() {
+  return (
+    <div className="App">
+      
+      
+      <Header/>
+      <div className = "move">
+        <Searchbar/>
+        <Carousel />
+      </div>
+      <Features/>
+    </div>
+  );
+}
+
+export default Home;
