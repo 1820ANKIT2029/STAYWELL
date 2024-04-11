411bdbf1a8b25131a13fa6b629b67ad027401822
aloknegi786 <144169514+aloknegi786@users.noreply.github.com>
2024-04-11 14:36:24 +0530

    adding Login/signup pages

---
+++ frontend/src/components/register/Register.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Register.css';
+import { FaUser, FaLock} from "react-icons/fa6";
+
+const Register = () =>{
+    return (
+        <>
+        <div className='wrapper'>
+            <form action="">
+                <h1>Signup</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='Name' required/>
+                </div>
+                <select name="gender" className='gender' id='box'>
+                        <option value="">Please select your gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                </select>
+                
+                <div className="input-box">
+                    <input type="text" placeholder='username' required/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='password'/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='confirm password'/>
+                </div>
+                <div className="but">
+                    <button type= "input-box" id='submit'>Login</button>
+                </div>
+                
+            </form>
+        </div>
+        </>
+    );
+};
+
+export default Register;
