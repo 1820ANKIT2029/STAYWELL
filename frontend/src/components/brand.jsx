3efbcc03c8ca3243820c7dd33fd6b1d44279d826
aloknegi786 <144169514+aloknegi786@users.noreply.github.com>
2024-04-11 14:34:26 +0530

    Add files via upload

---
+++ frontend/src/components/brand.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './register/Register.css';
+// import './loginForm/LoginForm';
+
+const LogoIcon = () =>{
+    return (
+        <>
+            <div className="image">
+                <img src="./assets/staywell.png" alt="" className="brand" />
+            </div>
+        </>
+    );
+};
+
+export default LogoIcon;
