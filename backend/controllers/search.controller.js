78ab87f0a76f95638ddb2548bcf725af3f398f0f
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-10-26 15:04:05 +0530

    C:/Program Files/Git/search/location filter updated

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,26 +1,49 @@
 const location = async (req, res, next) => {
     console.log("location search working");
     const loc = req.query.loc;
-    const url = 'https://booking-com.p.rapidapi.com/v1/hotels/locations?name='+loc+'&locale=en-gb';
-
-    const option = {
-        method: 'GET', 
-        headers: {
-            'X-RapidAPI-Key': process.env.BOOKING_API_KEY,
-		    'X-RapidAPI-Host': process.env.BOOKING_API_HOST
-        }
-    };
-
-    try{
-        const response = await fetch(url , option);
-        const result = await response.json();
-        res.status(200).json(result);
-        console.log("location send", loc);
-    }catch(error){
-        console.log("Error in location controller", error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if(!loc){
+        const e = {error: "Invalid Location (loc)"};
+        res.status(500).json(e);
+        console.log("Location error: ", e);
     }
-    console.log("Location error ended");
+    else{
+        const url = 'https://booking-com.p.rapidapi.com/v1/hotels/locations?name='+loc+'&locale=en-gb';
+
+        const option = {
+            method: 'GET', 
+            headers: {
+                'X-RapidAPI-Key': process.env.BOOKING_API_KEY,
+                'X-RapidAPI-Host': process.env.BOOKING_API_HOST
+            }
+        };
+
+        try{
+            const response = await fetch(url , option);
+            const result = await response.json();
+            let data = []
+            for(let location of result){
+                const l = {
+                    "latitude": location["latitude"],
+                    "longitude": location["longitude"],
+                    "timezone": location["timezone"],
+                    "city_name": location["city_name"],
+                    "region": location["region"],
+                    "country": location["country"],
+                    "image_url": location["image_url"]
+                }
+                data.push(l);
+            }
+            res.status(200).json(data);
+            console.log("location send: ", loc);
+
+        }catch(error){
+            console.log("Error in location controller", error);
+            const e = { error: "Internal Server Error" };
+            res.status(500).json(e);
+            console.log("Location error: ", e);
+        }
+    }
+    
 };
 
 const hotelsByCoordinate = async(req, res, next)=>{
