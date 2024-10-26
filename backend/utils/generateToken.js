b24e0afaa935d70e56a8cfd5f8e3738822de34fb
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-10-26 15:42:30 +0530

    remember me auth/login improved

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const generateTokenAndSetCookie = (userId, res)=>{
+const generateTokenAndSetCookie = (userId, rememberMe, res)=>{
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: '15d'
+        expiresIn: rememberMe?'30d':'1d',
     });
 
     res.cookie('jwt', token, {
-        maxAge: 15*24*60*60*1000, 
+        maxAge: rememberMe?30*24*60*60*1000:1*24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict"
+        sameSite: "None"
     });
 };
 
